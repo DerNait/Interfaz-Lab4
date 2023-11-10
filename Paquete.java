5e5b5e0e5388368e2335bf14403279344f9c8790
DerNait <75186178+DerNait@users.noreply.github.com>
2023-11-10 08:39:32 -0600

    Add files via upload

---
+++ Paquete.java
@@ -0,0 +1,12 @@
+public interface Paquete{
+
+    public void reservaViaje();
+
+    public void confirmacionPago();
+
+    public int seleccionAsiento();
+
+    public int cantidadMaletas();
+
+    public void modificarPerfil();
+}
